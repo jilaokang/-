27e43b1aa0ff6c9256af6bd456a1fb9630fae259
钱博康 <516764216@qq.com>
2018-01-05 16:50:13 +0800

    cc

--- docs/html/03.bus/js/teacher/json.js
+++ docs/html/03.bus/js/teacher/json.js
@@ -4,10 +4,8 @@ $(document).ready(function () {
     $.getJSON("data.json", function (result) {
         $.each(result, function (i,val) {
             json = this;
-            console.log("");
-            console.log("This is JSON:");
+            console.log("**********  JSON  **********");
             console.log(json);
-
         })
     });
 });
