89c0dd64482a858646e8715089fbaa711e4ab226
钱博康 <aa@Kaso.local>
2018-01-08 00:17:27 +0800

    cc

--- docs/html/03.bus/js/teacher/json.js
+++ docs/html/03.bus/js/teacher/json.js
@@ -1,4 +1,4 @@
-let json = new Promise((resolve, reject) => {
+let sb = new Promise((resolve, reject) => {
     try {
         $.getJSON("data.json", function (result) {
             resolve(result)
