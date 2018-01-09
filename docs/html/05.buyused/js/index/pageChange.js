5d613d04fc764fecf4460d58c9764964e13c1d6f
钱博康 <516764216@qq.com>
2018-01-09 14:59:04 +0800

    cc

---
+++ docs/html/05.buyused/js/index/pageChange.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#pages2").hide();
+    $("#icon1").addClass("activeIcon");
+    $("#icon1").click(function () {
+        $("#icon1").addClass("activeIcon");
+        $("#icon2").removeClass("activeIcon");
+        $("#pages1").show();
+        $("#pages2").hide();
+        console.log("icon1 clicked")
+    });
+    $("#icon2").click(function () {
+        $("#icon2").addClass("activeIcon");
+        $("#icon1").removeClass("activeIcon");
+        $("#pages1").hide();
+        $("#pages2").show();
+        console.log("icon2 clicked")
+    })
+});
