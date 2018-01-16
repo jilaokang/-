d0562633cab06082a6760019225d7a0ef555481a
钱博康 <aa@Kaso.local>
2018-01-16 19:20:35 +0800

    cc

--- docs/html/05.buyused/js/index/pageChange.js
+++ docs/html/05.buyused/js/index/pageChange.js
@@ -1,46 +1,50 @@
 $(document).ready(function () {
+    activePage = 1;
 
+    window.onload = function () {
 
-
-    $("#icon1").click(function () {
-        $("#icon1").addClass("activeIcon");
-        $("#icon2").removeClass("activeIcon");
-        $("#pages1").show();
-        $("#pages2").hide();
-        console.log("icon1 clicked")
-    });
-    
-    $("#icon2").click(function () {
-        $("#icon2").addClass("activeIcon");
-        $("#icon1").removeClass("activeIcon");
-        $("#pages1").hide();
-        $("#pages2").show();
-
-        console.log("icon2 clicked")
-    })
-
-
-
-    var a = 0;
-
-    $("#menuButton").click(function () {
-        a++;
-        a = a % 2;
-        if (a === 1) {
-            $("#rightNav").slideDown("800");
-        } else {
-            $("#rightNav").slideUp("800");
+        function iconChange(){            
+            var href = window.location.href;
+            arr = href.split("?");
+            console.log(arr)
+            if (arr[1]) {
+                activePage = 2;
+            }
         }
-        console.log(a);
-    })
+        iconChange();
 
+        $("#icon1").click(function () {
+            $("#icon1").addClass("activeIcon");
+            $("#icon2").removeClass("activeIcon");
+            $("#pages1").show();
+            $("#pages2").hide();
+            console.log("icon1 clicked")
+        });
 
+        $("#icon2").click(function () {
+            $("#icon2").addClass("activeIcon");
+            $("#icon1").removeClass("activeIcon");
+            $("#pages1").hide();
+            $("#pages2").show();
+            console.log("icon2 clicked")
+        })
 
-    if (activePage === 2) {
-        $("#icon2").click();
+        var a = 0;
+        $("#menuButton").click(function () {
+            a++;
+            a = a % 2;
+            if (a === 1) {
+                $("#rightNav").slideDown("800");
+            } else {
+                $("#rightNav").slideUp("800");
+            }
+            console.log(a);
+        })
+
+        if (activePage === 2) {
+            $("#icon2").click();
+        } else if (activePage === 1) {
+            $("#icon1").click();
+        }
     }
-    if (activePage === 1) {
-        $("#icon1").click();
-    }
-
 });
